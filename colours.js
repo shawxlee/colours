d29aa5410d79a26294b18699d1c517722019331e
shawxlee <896936846@qq.com>
2019-04-13 08:38:07 +0800

    Create "colours", add new files

---
+++ colours.js
@@ -0,0 +1,31 @@
+$(function () {
+    $("a").click(function(){
+        window.location.reload();
+    });
+    $("#changeColor").click(function(event){
+        var icVal = $("#inputColor").val();
+        function testPatt(v) {
+            var patt = /^#([0-9]|[a-zA-Z]){6}$/;
+            return patt.test(v);
+        };
+        if (testPatt(icVal)) {
+            $("#currentColor").text(icVal);
+            $("#currentColor").css("color", icVal);
+            $("body").css({
+                "background-color": icVal,
+                "color": icVal,
+            });
+            $("td").css("border-color", icVal);
+        }
+        else {
+            $("input").css("background-color", "red");
+        };
+        event.stopPropagation();
+    });
+    $("#inputColor").focus(function(){       
+        $("input").css("background-color", "");
+    });
+    $(document).click(function(){
+        $("input").css("background-color", "");
+    });
+});
