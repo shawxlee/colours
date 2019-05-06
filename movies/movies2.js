d6e1dceca790c5b48a2819fd0189be7e419cfbf9
shawxlee <shawxlee93@gmail.com>
2019-05-06 23:23:16 +0800

    Delete; Modify; Rename; Add new files

--- movies/movies2.js
+++ movies/movies2.js
@@ -1,151 +1,157 @@
-$(function () {
-	$.ajax({
-		url: "movies.json",
-		type: "GET",
-		async: false,
-		dataType: "json",
-		success (data) {
-			vm.films = data;
-			$("#loadingBg").remove();
-			$("#topDivider, #detailModel, #miniModel, #seriesModel, #bottomDivider, #shortCut").removeClass("d-none");
-		},
-		error () {
-			$("#loadingInfo").html("<i class='fas fa-exclamation-circle'></i><br><br><span>加载失败！</span>");
-		}
-	});
-
-	!function (win, lib) {
-		var timer,
-		doc = win.document,
-		docElem = doc.documentElement,
-		vpMeta = doc.querySelector('meta[name="viewport"]'),
-		dpr = 0,
-		scale = 0,
-		flexible = lib.flexible || (lib.flexible = {});
-
-		// 设置了 viewport meta
-		if (vpMeta) {
-			console.warn("将根据已有的meta标签来设置缩放比例");
-			var initial = vpMeta.getAttribute("content").match(/initial\-scale=([\d\.]+)/);
-			if (initial) {
+// 利用js计算当前设备的DPR，动态设置html的font-size，利用css的选择器根据DPR来设置不同DPR下的字体大小
+!function (win, lib) {
+	var timer,
+	doc = win.document,
+	docElem = doc.documentElement,
+	vpMeta = doc.querySelector('meta[name="viewport"]'),
+	dpr = 0,
+	scale = 0,
+	flexible = lib.flexible || (lib.flexible = {});
+	// 设置了 viewport meta
+	if (vpMeta) {
+		console.warn("将根据已有的meta标签来设置缩放比例");
+		var initial = vpMeta.getAttribute("content").match(/initial\-scale=([\d\.]+)/);
+		if (initial) {
 				scale = parseFloat(initial[1]); // 已设置的 initialScale
 				dpr = parseInt(1 / scale);      // 设备像素比 devicePixelRatio
 			}
 		}
-
 		function setFontSize() {
 			var winWidth = docElem.getBoundingClientRect().width;
 			if (winWidth / dpr > 540) {
 				(winWidth = 540 * dpr);
 			}
-			// 根节点 fontSize 根据宽度决定
-			var baseSize = winWidth / 25;
-			docElem.style.fontSize = baseSize + "px";
-			flexible.rem = win.rem = baseSize;
+		// 根节点 fontSize 根据宽度决定
+		var baseSize = winWidth / 25;
+		docElem.style.fontSize = baseSize + "px";
+		flexible.rem = win.rem = baseSize;
+	}
+	// 调整窗口时重置
+	win.addEventListener("resize", function() {
+		clearTimeout(timer);
+		timer = setTimeout(setFontSize, 300);
+	}, false);
+	// orientationchange 时也需要重算下
+	win.addEventListener("orientationchange", function() {
+		clearTimeout(timer);
+		timer = setTimeout(setFontSize, 300);
+	}, false);
+	// pageshow
+	// keyword: 倒退 缓存相关
+	win.addEventListener("pageshow", function(e) {
+		if (e.persisted) {
+			clearTimeout(timer);
+			timer = setTimeout(setFontSize, 300);
 		}
-
-		// 调整窗口时重置
-		win.addEventListener("resize", function() {
-			clearTimeout(timer);
-			timer = setTimeout(setFontSize, 300);
-		}, false);
-
-		// orientationchange 时也需要重算下
-		win.addEventListener("orientationchange", function() {
-			clearTimeout(timer);
-			timer = setTimeout(setFontSize, 300);
-		}, false);
-
-		// pageshow
-		// keyword: 倒退 缓存相关
-		win.addEventListener("pageshow", function(e) {
-			if (e.persisted) {
-				clearTimeout(timer);
-				timer = setTimeout(setFontSize, 300);
-			}
-		}, false);
-
-		// 设置基准字体
-		if ("complete" === doc.readyState) {
+	}, false);
+	// 设置基准字体
+	if ("complete" === doc.readyState) {
+		doc.body.style.fontSize = 12 * dpr + "px";
+	} else {
+		doc.addEventListener("DOMContentLoaded", function() {
 			doc.body.style.fontSize = 12 * dpr + "px";
-		} else {
-			doc.addEventListener("DOMContentLoaded", function() {
-				doc.body.style.fontSize = 12 * dpr + "px";
-			}, false);
+		}, false);
+	}
+	setFontSize();
+	flexible.dpr = win.dpr = dpr;
+	flexible.refreshRem = setFontSize;
+	flexible.rem2px = function(d) {
+		var c = parseFloat(d) * this.rem;
+		if ("string" == typeof d && d.match(/rem$/)) {
+			c += "px";
 		}
-
-		setFontSize();
-		flexible.dpr = win.dpr = dpr;
-		flexible.refreshRem = setFontSize;
-
-		flexible.rem2px = function(d) {
-			var c = parseFloat(d) * this.rem;
-			if ("string" == typeof d && d.match(/rem$/)) {
-				c += "px";
-			}
-			return c;
-		};
-
-		flexible.px2rem = function(d) {
-			var c = parseFloat(d) / this.rem;
-			if ("string" == typeof d && d.match(/px$/)) {
-				c += "rem";
-			}
-			return c;
+		return c;
+	};
+	flexible.px2rem = function(d) {
+		var c = parseFloat(d) / this.rem;
+		if ("string" == typeof d && d.match(/px$/)) {
+			c += "rem";
 		}
-	} (window, window.lib || (window.lib = {}));
+		return c;
+	}
+} (window, window.lib || (window.lib = {}));
 
-	$("#screenCollapse").on('show.bs.collapse hidden.bs.collapse', function () {
-		$("#screenToggler").toggleClass("togglerColor");
+// 主要jquery脚本
+$(function () {
+	// 给json地址添加随机参数
+	var date = new Date();
+	var timer = date.getTime();
+	var reUrl = "https://github.com/shawxlee/practice-works/blob/master/movies/movies.json?t=" + timer;
+	// 首次加载数据
+	loadData();
+	function loadData() {
+		$.ajax({
+			url: reUrl,
+			type: "GET",
+			async: false,
+			cache: false,
+			dataType: "json",
+			success (result) {
+				vm.films = result;
+				$("#loadingBg").remove();
+			},
+			error () {
+				$("#loadingInfo").html("<img src='error.png'><br><br><span>加载失败！</span><br><br><button type='button' class='btn btn-outline-primary'>重新加载</button>");
+			}
+		});
+	}
+	$("#loadingInfo button").click(function () {
+		loadData();
 	});
+
+	// 折叠面板
+	$("#screenCollapse").on('show.bs.collapse hidden.bs.collapse', function () {
+		$(".screenToggler").toggleClass("togglerColor");
+	});
+	$(".tab-content, .bottomDivider, .shortCut, .footNav").click(function () {
+		$("#screenCollapse").collapse('hide');
+	});
+
+	// 搜索框
 	$("#searchButton").click(function () {
 		var sb = $("#searchButton");
-		var si = $("#searchInput");
-		var sf = $("#searchForm");
+		var si = $(".searchInput");
 		if (!si.hasClass("inputTransition")) {
-			sb.css("color", "#4F4F4F");
+			sb.css("color", "#363636");
 			si.addClass("inputTransition");
-			sf.addClass("formTransition");
 		} else {
 			sb.attr("type", "submit");
 		}
 	});
-	$(".navbar-brand").click(function(){
-		$("#searchForm").removeClass("formTransition");
-		$("#searchInput").removeClass("inputTransition");
+	$(".navbar-brand").click(function () {
+		$(".searchInput").removeClass("inputTransition");
 		$("#searchButton").css("color", "#A9A9A9");
 		$("#searchButton").attr("type", "button");
 	});
-	$("#topDivider, #detailModel, #miniModel, #seriesModel, #bottomDivider, #shortCut").click(function () {
-		$("#screenCollapse").collapse('hide');
-	});
 
-	$("#toTop").click(function () {
-		$('html, body').animate({ scrollTop: 0 }, 500, function () {
-			// body...
+	// 更新数据并显示状态
+	function reloadData() {
+		$("#reloadDivider").animate({ marginTop: "3rem" });
+		$.get(reUrl, function (data, status) {
+			if (status == "success") {
+				vm.films = data;
+				$("#reloadDivider").html("<hr>&nbsp;&nbsp; 更新成功 <i class='fas fa-check-circle'></i> &nbsp;&nbsp;<hr>");
+			} else if (status == "notmodified") {
+				$("#reloadDivider").html("<hr>&nbsp;&nbsp; 没有更新 <i class='fas fa-minus-circle'></i> &nbsp;&nbsp;<hr>");
+			} else {
+				$("#reloadDivider").html("<hr>&nbsp;&nbsp; 更新失败 <i class='fas fa-exclamation-circle'></i> &nbsp;&nbsp;<hr>");
+			}
+		}, "json");
+		setTimeout(function () {
+			$("#reloadDivider").animate({ marginTop: "0.4rem" });
 		});
-	});
-	$("#reLoad").click(function () {
-		function getData() {
-			$.ajax({
-				url: "movies.json",
-				type: "GET",
-				async: false,
-				cache: false,
-				dataType: "json",
-				success (data) {
-					vm.films = data;
-					$("#loadingBg").remove();
-					$("#topDivider, #detailModel, #miniModel, #seriesModel, #bottomDivider, #shortCut").removeClass("d-none");
-				},
-				error () {
-					$("#loadingInfo").html("<i class='fas fa-exclamation-circle'></i><br><br><span>加载失败！</span>");
-				}
-			});
-		}
+		setTimeout(function () {
+			$("#reloadDivider").html("<hr>&nbsp;&nbsp; 正在更新 <i class='fas fa-sync-alt'></i> &nbsp;&nbsp;<hr>");
+		});
+	}
+
+	// 返回顶部
+	$("#toTop").click(function () {
+		$('html, body').animate({ scrollTop: 0 }, 500, reloadData);
 	});
 });
 
+// vue实例
 var vm = new Vue({
 	el: '#app',
 	data: {
@@ -171,6 +177,7 @@ var vm = new Vue({
 		films: [],
 	},
 	methods: {
+		// 过滤，排序
 		filterSort (films) {
 			if (this.sortOrder == 1) {
 				return this.films.sort(function (a,b) {
@@ -186,6 +193,7 @@ var vm = new Vue({
 				});
 			}
 		},
+		// 分数分级显示不同颜色
 		scoreColor (item) {
 			if (item.score >= 9) {
 				return '#A020F0';
